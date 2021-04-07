b0195221b7352b2e0cb3ece377c3d226c004ef08
DwarfPorter <mrvlmor@gmail.com>
2021-04-07 20:55:46 +0300

    поправил макет

--- app/src/main/java/ru/geekbrains/myapplication/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package ru.geekbrains.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+
+    /// Супер пупер метод
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ed = findViewById<EditText>(R.id.editText)
     }
 }
